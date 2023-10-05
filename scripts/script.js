efb8ab0d8da6f94faef912fea01375cdce4b155a
Viktor Kononov <viktor1998kononov@gmail.com>
2023-10-05 20:29:16 +0300

    added styles for swapping language

--- scripts/script.js
+++ scripts/script.js
@@ -35,3 +35,18 @@ function hidePopup() {
     $('.popupWrapper').addClass('hidden');
 }
 
+$('.langBlock').click(function (event) {
+
+    //* add language swap here
+    // if there's another page for each lang, there's no need of all of it
+
+    $('.langBlock .btn').each(function (i, elem) {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            return false;
+        }
+    });
+
+    let btn = event.target.id;
+    $('#' + btn).addClass('active');
+});
