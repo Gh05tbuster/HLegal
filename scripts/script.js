c43cabceff696dbce8f3b778c10a78967a9a966e
Viktor Kononov <64473033+Gh05tbuster@users.noreply.github.com>
2023-10-19 22:47:05 +0300

    Home page (#7)
    * created home page
    
    * replaced basic nav in burger with additional to avoid z-index problem

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
     $('.burger').click(function (event) {
-        showPopup();
+        showPopupNav();
+    });
+
+    $('.consultation').click(function (event) {
+        showPopupFrom();
     });
 
     $('.closeBtn').click(function () {
@@ -19,19 +23,81 @@ $(document).ready(function () {
         hidePopup();
     });
 
-    $('.cardSlider').slick({
-        dots: true,
-        infinite: false,
-        slidesToShow: 1,
-    });
+    $('.cardSlider').owlCarousel({
+        loop: false,
+        margin: 20,
+        items: 1,
+    })
 });
 
-function showPopup() {
-    $('.headerNav').addClass('floating');
+//* for nav inside header (issues with z-index)
+
+// function showPopup() {
+//     $('.headerNav').addClass('floating');
+//     $('.popupWrapper').removeClass('hidden');
+// }
+// function hidePopup() {
+//     $('.headerNav').removeClass('floating');
+//     $('.popupWrapper').addClass('hidden');
+// }
+
+//* for nav inside popup
+
+function showPopupNav() {
+    $('.burgerNav').removeClass('hidden');
     $('.popupWrapper').removeClass('hidden');
 }
-function hidePopup() {
-    $('.headerNav').removeClass('floating');
-    $('.popupWrapper').addClass('hidden');
+
+function showPopupForm() {
+    $('.burgerNav').removeClass('hidden');
+    $('.contactForm').removeClass('hidden');
 }
 
+function hidePopup() {
+    $('.burgerNav').addClass('hidden');
+    $('.popupWrapper').addClass('hidden');
+    $('.contactFrom').addClass('hidden');
+}
+
+function scrollDown(elem) {
+    event.preventDefault();
+    const target = document.querySelector(elem.hash);
+    window.scrollTo({
+        top: target.offsetTop - 30,
+        behavior: "smooth"
+    });
+}
+
+$('.langBlock .btn').click(function (event) {
+
+    //* add language swap here
+    // if there's another page for each lang, there's no need of all of it
+
+    $('.langBlock .btn').each(function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            return false;
+        }
+    });
+
+    let btn = event.target.id;
+    $('#' + btn).addClass('active');
+});
+
+$('.pills .pillsTab .title').click(function (event) {
+    let pi;
+    $('.pills .pillsTab .title').each(function (i, el) {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            pi = el.id;
+            $('.pills .tabContent .' + pi).removeClass('active');
+            $('.pills .tabContent .' + pi).addClass('hidden');
+            return false;
+        }
+    });
+
+    pi = event.target.id;
+    $('.pills .pillsTab #' + pi).addClass('active');
+    $('.pills .tabContent .' + pi).addClass('active');
+    $('.pills .tabContent .' + pi).removeClass('hidden');
+});
